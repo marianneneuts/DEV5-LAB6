812427fc84c59ea98e29f37d2e73edf3f29c4f62
Marianne <marianneneuts@gmail.com>
2022-11-21 09:02:53 +0100

    feat: show users and text on App.vue

--- lab6/src/components/Chat.vue
+++ lab6/src/components/Chat.vue
@@ -1,13 +1,35 @@
 <script setup>
+    import { onMounted, reactive, ref } from 'vue';
 
+    let chats = reactive({chats: []});
+    let text = ref('');
+
+    // onMounted
+    onMounted (() => {
+        const apiUrl = 'https://lab5-p379.onrender.com/api/v1/messages/';
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(chats);
+                chats.chats = data;
+            })
+    });
 </script>
 
 <template>
     <div>
-        <h1>Chat</h1>
-    </div>
+        <div class="chat">
+            <ul>
+                <li v-for="chat in chats.chats" :key="chats.id">
+                    <p><strong><mark>{{ chat.user }}</mark></strong>: {{ chat.text }}</p>
+                </li>
+            </ul>
+        </div>
+     </div>
 </template>
 
 <style scoped>
-
+    ul {
+        list-style: none;
+    }
 </style>
